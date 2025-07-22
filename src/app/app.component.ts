8368f1a9ea754f4afa91a0752a4459187ebfa28b
Juliana de Carvalho <jucarvalho@outlook.com.br>
2025-07-22 19:32:32 -0300

    page home adicionada ao menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   public appPages = [
+    { title: 'Home', url: '/home', icon: 'home' },
     { title: 'Pokedex', url: '/pokedex', icon: 'planet' }
   ];
 
